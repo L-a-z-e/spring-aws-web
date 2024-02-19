dee6ff2e3aefeaebcb14c4888637f0d1b494b1a7
L-a-z-e <yysi8771@gmail.com>
2024-02-20 00:22:53 +0900

    first commit

---
+++ src/main/java/com/laze/springawsweb/SpringAwsWebApplication.java
@@ -0,0 +1,13 @@
+package com.laze.springawsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsWebApplication.class, args);
+    }
+
+}
